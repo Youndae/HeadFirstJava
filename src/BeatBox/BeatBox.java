1df8147994afc28ea59944eeadb1611cbeb93560
Youndae <namyoundae@gmail.com>
2020-10-16 01:10:59 +0900

    10/15/2

--- src/BeatBox/BeatBox.java
+++ src/BeatBox/BeatBox.java
@@ -3,6 +3,8 @@ package BeatBox;
 import java.awt.*;
 import javax.swing.*;
 import javax.sound.midi.*;
+import java.io.File;
+import java.io.*;
 import java.util.*;
 import java.awt.event.*;
 
@@ -175,5 +177,48 @@ public class BeatBox {
             e.printStackTrace();
         }
         return event;
+    }
+
+    public class MySendListener implements ActionListener{
+        public void actionPerformed(ActionEvent a){
+            boolean[] checkboxState = new boolean[256];
+            for(int i = 0; i < 256; i++){
+                JCheckBox check = (JCheckBox) checkboxList.get(i);
+                if(check.isSelected()){
+                    checkboxState[i] = true;
+                }
+            }
+
+            try{
+                FileOutputStream fileStream = new FileOutputStream(new File("checkbox.ser"));
+                ObjectOutputStream os = new ObjectOutputStream(fileStream);
+                os.writeObject(checkboxState);
+            }catch(Exception ex){
+                ex.printStackTrace();
+            }
+        }
+    }
+
+    public class MyReadInListener implements ActionListener{
+        public void actionPerformed(ActionEvent a){
+            boolean[] checkboxState = null;
+            try{
+                FileInputStream fileIn = new FileInputStream(new File("checkbox.ser"));
+                ObjectInputStream is = new ObjectInputStream(fileIn);
+                checkboxState = (boolean[]) is.readObject();
+            }catch(Exception ex){
+                ex.printStackTrace();
+            }
+            for(int i = 0; i < 256; i++){
+                JCheckBox check = (JCheckBox) checkboxList.get(i);
+                if(checkboxState[i]){
+                    check.setSelected(true);
+                }else{
+                    check.setSelected(false);
+                }
+            }
+            sequencer.stop();
+            buildTrackAndStart();
+        }
     }
 }
